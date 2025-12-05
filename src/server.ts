7f034946a08418cd5f58f8dda57a076aab98de27
mmhasannextb6 <mmhasan.next.b6@gmail.com>
2025-12-05 21:48:19 +0600

    api

--- src/server.ts
+++ src/server.ts
@@ -118,6 +118,58 @@ if(result.rows.length===0){
 }
 })
 
+
+//update
+app.put("/users/:id", async(req:Request, res:Response)=>{
+  const {name, email} = req.body
+try{
+const result = await pool.query(`UPDATE users SET name=$1, email=$2 WHERE id=$3  RETURNING *`,
+[name, email, req.params.id])
+
+if(result.rows.length===0){
+  throw new Error("user not found")
+}else{
+  res.status(200).send({
+  success:true,
+  message:'user updated successfully',
+  data:result.rows[0]
+})
+}
+}catch(err){
+  res.status(500).json({
+      success:false,
+      message:"user not found",
+      details:err
+    })
+}
+})
+
+
+//update
+app.delete("/users/:id", async(req:Request, res:Response)=>{
+try{
+const result = await pool.query(`DELETE FROM users WHERE id=$1`,
+[req.params.id])
+
+if(result.rowCount===0){
+  throw new Error("user not found")
+}else{
+  res.status(200).send({
+  success:true,
+  message:'user deleted successfully',
+  data:result.rows
+})
+}
+}catch(err){
+  res.status(500).json({
+      success:false,
+      message:"user not found",
+      details:err
+    })
+}
+})
+
+
 app.listen(5000, () => {
   console.log(`Example app listening on port ${process.env.port}`)
 })
